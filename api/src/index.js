4be8297e3164ad3f9d94edbb603d9e687d7eb565
jymassol.netmessage <jymassol@netmessage.com>
2018-03-21 09:44:57 +0100

    User authentication

--- api/src/index.js
+++ api/src/index.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import path from "path";
-//import mongoose from "mongoose";
-//import bodyParser from "body-parser";
-//import dotenv from "dotenv";
-//import Promise from "bluebird";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import Promise from "bluebird";
 
-//import auth from "./routes/auth";
+import auth from "./routes/auth";
 //import users from "./routes/users";
 //import books from "./routes/books";
 
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid credentials"} });
-});
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.listen(3334, () => console.log('Running on localhost:3334'));
+app.listen(3335, () => console.log('Running on localhost:3335'));
